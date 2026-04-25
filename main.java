d9a0d16e78f34dc3507f60dbaae69030664fe19a
Dima <Dima@DESKTOP-058A3R7>
2026-04-25 17:09:10 +0300

    gitignore added

---
+++ main.java
@@ -0,0 +1,14 @@
+
+    import Database.DatabaseManager;
+
+public class main {
+    public static void main(String[] args) {
+        
+        // This will test your connection and create the database!
+        DatabaseManager.createTables();
+
+    }
+}
+    
+    
+
